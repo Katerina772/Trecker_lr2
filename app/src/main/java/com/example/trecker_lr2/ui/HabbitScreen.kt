aa9b7d3d09947b6d034f29d96e81c1dbb7e6cb0b
Katerina772 <katapalamarcuk530@gmail.com>
2026-04-29 16:38:48 +0300

    Added progress bar

--- app/src/main/java/com/example/trecker_lr2/ui/HabbitScreen.kt
+++ app/src/main/java/com/example/trecker_lr2/ui/HabbitScreen.kt
@@ -14,15 +14,31 @@ fun HabitScreen(viewModel: HabitViewModel) {
     val habits by viewModel.habits.collectAsState()
     val completed by viewModel.completedCount.collectAsState()
 
+    // Розрахунок прогресу
+    val progress = if (habits.isNotEmpty()) {
+        completed.toFloat() / habits.size
+    } else 0f
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
     ) {
 
+
         Text(
             text = "Виконано сьогодні: $completed з ${habits.size}",
             style = MaterialTheme.typography.titleLarge
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+
+        LinearProgressIndicator(
+            progress = progress,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(8.dp)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
